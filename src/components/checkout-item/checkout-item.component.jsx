ccd661e37212a4ce83154f1ed843d9cf123cdcf0
Nguyễn Công <nguyencong@Nguyens-MacBook-Air.local>
2020-07-30 18:55:58 +0700

    done checkout and add createStructuredCreator

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,17 +1,40 @@
-import React from "react"
+import React from "react";
+import { connect } from "react-redux";
+import {
+  clearItemFromCart,
+  removeItemFromCart,
+} from "../../redux/cart/cart.actions";
+import "./checkout-item.styles.scss";
 
-import "./checkout-item.styles.scss"
-
-const CheckoutItem = ({cartItem : {name , imageUrl , price , quantity}}) => (
+const CheckoutItem = ({ cartItem, clearItemFromCart, removeItemFromCart }) => {
+  const { name, imageUrl, price, quantity } = cartItem;
+  return (
     <div className="checkout-item">
-        <div className="image-container">
-            <img src={imageUrl} alt="item" />
+      <div className="image-container">
+        <img src={imageUrl} alt="item" />
+      </div>
+      <span className="name">{name}</span>
+      <span className="quantity">
+        <div className="arrow" onClick={() => removeItemFromCart(cartItem)}>
+          &#10094;
         </div>
-        <span className="name">{name}</span>
-        <span className="quantity">{quantity}</span>
-        <span className="price">{price}</span>
-        <div className="remove-button">&#10005;</div>
+        <span className="value">{quantity}</span>
+        <div className="arrow"> &#10095;</div>
+      </span>
+      <span className="price">{price}</span>
+      <div
+        className="remove-button"
+        onClick={() => clearItemFromCart(cartItem)}
+      >
+        &#10005;
+      </div>
     </div>
-)
+  );
+};
 
-export default CheckoutItem;
+const mapDispatchToProp = (dispatch) => ({
+  clearItemFromCart: (item) => dispatch(clearItemFromCart(item)),
+  removeItemFromCart: (item) => dispatch(removeItemFromCart(item)),
+});
+
+export default connect(null, mapDispatchToProp)(CheckoutItem);
